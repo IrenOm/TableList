71cdd3b3514972de6620f2088e8c83890bd4dd6a
Irena Omelana <124605706+IrenOm@users.noreply.github.com>
2023-05-04 22:59:22 +0300

    Add Movie

--- TableList/Controller/DetailViewController.swift
+++ TableList/Controller/DetailViewController.swift
@@ -9,8 +9,10 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
+    
     @IBOutlet weak var trackImageView: UIImageView!
     @IBOutlet weak var trackNameLable: UILabel!
+ 
     
     var song: Song!
     
@@ -27,14 +29,16 @@ class DetailViewController: UIViewController {
     }
     
 
-    /*
+    
     // MARK: - Navigation
-
+/*
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if let indexPath = tableView.indexPathForSelectedRow {
+            print(indexPath)
+            // Pass the selected object to the new view controller.
+            let detailVC?.song = songs[indexPath.row]
+        }
+ */
     }
-    */
-
-}
